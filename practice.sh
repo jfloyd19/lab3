078f30e270c1b387920f65ad6b64ae64852802b0
jfloyd19 <justin.floyd@colorado.edu>
2020-02-05 14:32:52 -0700

    updating all files for submission

--- practice.sh
+++ practice.sh
@@ -2,14 +2,15 @@
 # Author : Your Name
 # Date: Today's date
 # Script follows here:
-echo "Enter a number: "
-read numOne
-echo "Enter a second number: "
-read numTwo
-sum=$(($numOne + $numTwo))
-echo "The sum is : $sum"
-let prod=numOne*numTwo
-echo "The product is: $prodecho "File Name: $0""
-echo "Command Line Argument 1: $1"
+#echo "Enter a number: "
+#read numOne
+#echo "Enter a second number: "
+#read numTwo
+#sum=$(($numOne + $numTwo))
+#echo "The sum is : $sum"
+#let prod=numOne*numTwo
+#echo "The product is: $prodecho "File Name: $0""
+#echo "Command Line Argument 1: $1"
 echo "File Name: $0"
 echo "Command Line Argument 1: $1"
+grep $1 $2
